322df5404ec126761b683ff2e1d31564d5488287
ANYANTUDRE <121746248+ANYANTUDRE@users.noreply.github.com>
2024-06-14 20:35:49 +0100

    api

---
+++ api/app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+from langserve import add_routes
+from dotenv import load_dotenv
+import uvicorn
+import os
+
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+app = FastAPI(
+    title="Langchain server",
+    version="1.0",
+    description="API server"
+)
+
+# we're using 2 llms: qween2 and deepseek-coder:1.3b
+qween_model = Ollama(model="qwen2:0.5b")
+deepseek_model = Ollama(model="deepseek-coder:1.3b")
+
+# prompts templates
+prompt1 = ChatPromptTemplate.from_template("Write me a love poem about {topic} with 50 words")
+prompt2 = ChatPromptTemplate.from_template("Write me a python code to train a simple {topic} machine learning model")
+
+add_routes(
+    app,
+    prompt1 | qween_model,
+    path="/poem"
+)
+
+add_routes(
+    app,
+    prompt2 | deepseek_model,
+    path="/code"
+)
+
+if __name__=="__main__":
+    uvicorn.run(app, host="localhost", port=8000)
---
+++ api/client.py
@@ -0,0 +1,28 @@
+import requests
+import streamlit as st
+
+
+def get_qween_response(input_text):
+    response = requests.post("http://localhost:8000/poem/invoke", 
+                             json={"input": {"topic": input_text}})
+
+    return response.json()['output']
+
+
+def get_deepseek_response(input_text):
+    response = requests.post("http://localhost:8000/code/invoke", 
+                             json={"input": {"topic": input_text}})
+
+    return response.json()['output']
+
+
+st.title("Demo Qween2 and DeepSeekCoder")
+
+input_text = st.text_input("Write an poem topic here...")
+input_text2 = st.text_input("Write a model name here...")
+
+if input_text:
+    st.write(get_qween_response(input_text))
+
+if input_text2:
+    st.write(get_deepseek_response(input_text2))
